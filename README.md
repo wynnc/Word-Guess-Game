5a00d2e9546c156ba630ad20103fe04b7e5b57a9
Wynn Chaney <wynn.chaney@wanco.com>
2019-05-03 19:02:10 -0600

    Added notes to html, added html, added array to javascript file.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Word-Guess-Game
+Create array of words to guess based on chosen theme.
+OnKeyUp method will give either a letter in the word or end up in the guessed row. This will use an if else statement.
---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var wordList= [
+    "Journey",
+    "Blondie",
+    "Madonna",
+    "Michael Jackson",
+    "New Kids On the Block",
+    "Elton John",
+    "Lionel Richie",
+    "Culture Club",
+    "Reo Speedwagon",
+    "REM",
+    "U2"
+]
